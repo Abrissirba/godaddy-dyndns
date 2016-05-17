d53677f5396006d16f458a68bfc1b9b79bffdc59
Marcus Abrahamsson <marabr@solutionxperts.se>
2016-05-17 15:48:03 +0200

    changed script

--- godaddy-dyndns.py
+++ godaddy-dyndns.py
@@ -3,28 +3,59 @@
 import configparser
 import logging
 import sys
+import string
 
 import pif
 import pygodaddy
+import smtplib
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+username = ''
+password = ''
+SERVER = ""
+FROM = ""
+TO = ""
 
 logging.basicConfig(filename='godaddy-dyndns.log',
 		    format='%(asctime)s %(message)s',
 		    level=logging.INFO)   
-config = configparser.ConfigParser()
-config.read('godaddy-dyndns.conf')
+
+def sendEmail(body):
+    
+    SUBJECT = "Dynamic DNS Error - server adsync"
+
+    msg = MIMEMultipart()
+    msg['From'] = FROM
+    msg['To'] = TO
+    msg['Subject'] = SUBJECT
+    msg.attach(MIMEText(body, 'plain'))
+
+    text = msg.as_string()
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM, TO, text)
+    server.quit()
+    return;
 
 client = pygodaddy.GoDaddyClient()
-is_logged_in = client.login(config.get('godaddy', 'username'),
-			    config.get('godaddy', 'password'))
+is_logged_in = client.login(username, password)
 if not is_logged_in:
     logging.error('Login failed!')
     sys.exit(1)
-
+    
 for domain in client.find_domains():
     dns_records = list(client.find_dns_records(domain))
     public_ip = pif.get_public_ip()
-    logging.info("Domain '{0}' DNS records: {1}".format(domain, dns_records))
-    if public_ip != dns_records[0].value:
-        client.update_dns_record(domain, public_ip)
-        logging.info("Domain '{0}' public IP set to '{1}'".format(domain, public_ip))
+    
+    for record in dns_records:
+        if record.hostname == 'web':
+            hostname = record.hostname + '.' + domain
+            try:
+                updated = client.update_dns_record(hostname, public_ip)
+                if updated:
+                    logging.info("Domain '{0}' public IP set to '{1}'".format(hostname, public_ip))
+            except ValueError as e:
+                sendEmail(': ' + public_ip)
+                logging.info(e)
+
